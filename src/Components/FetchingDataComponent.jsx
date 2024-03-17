99618eb09d2ab0daac257c2e110856b9e46265a3
Vladyslav Yatsenko <107263996+Miksirist@users.noreply.github.com>
2024-03-17 22:45:55 +0200

    added await

--- src/Components/FetchingDataComponent.jsx
+++ src/Components/FetchingDataComponent.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function UsersFetcher() {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
